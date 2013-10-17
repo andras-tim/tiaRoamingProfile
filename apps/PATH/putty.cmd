db360e2dd714d7ee5a6b8dc0dee48c446691d727
Andras Tim <andras.tim@gmail.com>
2013-10-17 18:07:46 +0200

    apps: Added PuTTY and WinSCP runners with registry sync support

---
+++ apps/PATH/putty.cmd
@@ -0,0 +1,2 @@
+@set REG_KEY="HKEY_CURRENT_USER\Software\SimonTatham\PuTTY"
+@runWithRegistrySync "%trpAPPS%\putty\putty.exe" %*
---
+++ apps/PATH/winscp.cmd
@@ -0,0 +1,3 @@
+@set REG_KEY="HKEY_CURRENT_USER\Software\Martin Prikryl\WinSCP 2"
+@runWithRegistrySync "%trpAPPS%\winscp\winscp.exe" %*
+pause
